b8acbca74256117c856b29e062187ae654cb2bc2
Kevin Baguioso <tomfge09@yahoo.ca>
2023-11-20 23:23:17 -0700

    Initial files has been updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,14 @@
 
 import {Link} from 'react-router-dom'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import images from '../data'
+
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+// import { Carousel } from 'react-responsive-carousel';
+
+// import deviceWorx from '../img/Deviceworx portfolio.png'
+// import plantLife from '../img/Plant Life Website.png'
+// import weatherApp from '../img/Portfolio1Weather.png'
+// import comingSoon from '../img/Image Coming Soon.png'
+// import todoList from '../img/Todo list Project Website.png'
 
 
 export const Projects = () =>{
@@ -12,22 +18,92 @@ export const Projects = () =>{
         <div className="selection:bg-custom-color selection:text-white md:w-48  border-2 border-custom-color-two w-72 rounded-md h-16 flex items-center justify-center font">
           <h3 className="text-custom-color-two text-2xl tooling uppercase md:text-xl">Projects</h3>
         </div>
-        <div className='mt-32 '>
-          <Carousel className='mx-auto w-144'
-          autoPlay 
-          infiniteLoop
-          stopOnHover
-          >
-            {images.map((img) => 
-              <img 
-              className='h-144 object-cover'
-                src={img.imgURL} 
-                alt={img.alt}
-              />
-              )
-              
-            }
-          </Carousel>
+        <div className='mt-32 grid grid-cols-3 gap-10'>
+        <div className='border-2 border-custom-color-two p-5'>
+          <h2 className='font text-3xl text-custom-color-two'>Project One</h2>
+          <div className='flex justify-between text-custom-color-two my-2'>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>One</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Two</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Three</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Four</p>
+          </div>
+          <p className='w-96 mt-5 text-white '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed eros dui, scelerisque eu imperdiet at, luctus et leo. 
+          Donec ac dolor et eros rutrum scelerisque quis consequat urna. 
+          Praesent aliquet odio ut neque sodales luctus in at purus. 
+          Fusce ultricies ex ut orci vulputate, et faucibus purus porttitor.</p>
+        </div>
+        <div className='border-2 border-custom-color-two p-5'>
+          <h2 className='font text-3xl text-custom-color-two'>Project Two</h2>
+          <div className='flex justify-between text-custom-color-two my-2'>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>One</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Two</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Three</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Four</p>
+          </div>
+          <p className='w-96 mt-5 text-white '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed eros dui, scelerisque eu imperdiet at, luctus et leo. 
+          Donec ac dolor et eros rutrum scelerisque quis consequat urna. 
+          Praesent aliquet odio ut neque sodales luctus in at purus. 
+          Fusce ultricies ex ut orci vulputate, et faucibus purus porttitor.</p>
+        </div>
+        <div className='border-2 border-custom-color-two p-5'>
+          <h2 className='font text-3xl text-custom-color-two'>Project Three</h2>
+          <div className='flex justify-between text-custom-color-two my-2'>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>One</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Two</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Three</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Four</p>
+          </div>
+          <p className='w-96 mt-5 text-white '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed eros dui, scelerisque eu imperdiet at, luctus et leo. 
+          Donec ac dolor et eros rutrum scelerisque quis consequat urna. 
+          Praesent aliquet odio ut neque sodales luctus in at purus. 
+          Fusce ultricies ex ut orci vulputate, et faucibus purus porttitor.</p>
+        </div>
+        <div className='border-2 border-custom-color-two p-5'>
+          <h2 className='font text-3xl text-custom-color-two'>Project Four</h2>
+          <div className='flex justify-between text-custom-color-two my-2'>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>One</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Two</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Three</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Four</p>
+          </div>
+          <p className='w-96 mt-5 text-white '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed eros dui, scelerisque eu imperdiet at, luctus et leo. 
+          Donec ac dolor et eros rutrum scelerisque quis consequat urna. 
+          Praesent aliquet odio ut neque sodales luctus in at purus. 
+          Fusce ultricies ex ut orci vulputate, et faucibus purus porttitor.</p>
+        </div>
+        <div className='border-2 border-custom-color-two p-5'>
+          <h2 className='font text-3xl text-custom-color-two'>Project Five</h2>
+          <div className='flex justify-between text-custom-color-two my-2'>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>One</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Two</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Three</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Four</p>
+          </div>
+          <p className='w-96 mt-5 text-white '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed eros dui, scelerisque eu imperdiet at, luctus et leo. 
+          Donec ac dolor et eros rutrum scelerisque quis consequat urna. 
+          Praesent aliquet odio ut neque sodales luctus in at purus. 
+          Fusce ultricies ex ut orci vulputate, et faucibus purus porttitor.</p>
+        </div>
+        <div className='border-2 border-custom-color-two p-5'>
+          <h2 className='font text-3xl text-custom-color-two'>Project Six</h2>
+          <div className='flex justify-between text-custom-color-two my-2'>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>One</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Two</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Three</p>
+            <p className='border-2 border-custom-color-two p-1 text-xs'>Four</p>
+          </div>
+          <p className='w-96 mt-5 text-white '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed eros dui, scelerisque eu imperdiet at, luctus et leo. 
+          Donec ac dolor et eros rutrum scelerisque quis consequat urna. 
+          Praesent aliquet odio ut neque sodales luctus in at purus. 
+          Fusce ultricies ex ut orci vulputate, et faucibus purus porttitor.</p>
+        </div>
+          
         </div>
         <div className='selection:bg-custom-color selection:text-white my-32'>
           <Link className='btn border-2 border-custom-color-two tooling w-120 h-20 text-custom-color-two text-xl' to='/portfolio'>
--- src/components/Tools.js
+++ src/components/Tools.js
@@ -3,15 +3,15 @@ import { FaReact, FaHtml5, FaCss3Alt, FaBootstrap, FaNodeJs } from 'react-icons/
 import {BiLogoTailwindCss, BiLogoJavascript, BiLogoFigma, BiLogoWordpress} from 'react-icons/bi'
 
 export const Tools = () =>{
-  const customStyle = 'items-center flex justify-center md:h-10 md:w-24 h-12 w-40 mx-auto md:text-xs bg-btn-bg-color text-white rounded-md text-md font-bold '
+  const customStyle = 'items-center flex justify-center md:h-10 lg:w-32 lg:text-sm md:w-24 h-12 w-32 mx-auto md:text-xs bg-btn-bg-color text-white rounded-md text-sm font-bold '
   const iconStyles = 'text-custom-color text-3xl mr-2'
   return(
     <div className="h-96">
       <div className="grid justify-items-center">
-        <div className="md:w-48 border-custom-color-two  border-2 w-56 rounded-md h-16 flex items-center justify-center font">
+        <div className="md:w-48 border-custom-color-two sm:w-32 sm:mx-auto border-2 w-56 rounded-md h-16 flex items-center justify-center font">
           <h3 className="selection:bg-custom-color selection:text-white text-custom-color-two text-2xl tooling uppercase md:text-xl">Tooling</h3>
         </div>
-        <div className="selection:bg-custom-color bg-custom-color rounded-xl grid grid-cols-5 items-center gap-5 my-16 md:gap-3 sm:grid-cols-4 xs:grid-cols-3 w-custom p-5 h-48">
+        <div className="selection:bg-custom-color bg-custom-color rounded-xl grid grid-cols-5 items-center gap-1 my-16  md:w-120 md:h-60 sm:w-32  md:gap-2 xs:w-80 xs:h-72 xs:grid-cols-2 md:grid-cols-3 w-192 p-5 h-48">
           <div className={customStyle}><FaHtml5 className={iconStyles} /> HTML</div>
           <div className={customStyle}><FaCss3Alt className={iconStyles}/> CSS</div>
           <div className={customStyle}><FaBootstrap className={iconStyles}/> Bootstrap</div>
